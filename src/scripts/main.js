b5bb4b5f0f784341fb68dd7a2bf2e090b8eb72f1
Ivan Fragoso <ivan@paumari.com>
2019-10-18 15:04:18 +0200

    jQuery Task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,5 +10,38 @@ require('bootstrap');
  * 
  */
 $(document).ready(() => {
-    // 
+
+    /**
+     *  AJAX Request function at submitting the form
+     */
+    $('#modal-skill-test form').submit(function() {
+
+        // Comment for allow the form submitting
+        event.preventDefault();
+ 
+        // Get the form and the modal
+        var $form = $(this);
+        var $modal = $('#modal-skill-test');
+
+        // Define the form data
+        var $formData = {
+            'name'      : $('input#modal-skill-test-name').val(),
+            'surname'   : $('input#modal-skill-test-surname').val(),
+            'email'     : $('input#modal-skill-test-email').val()
+        };
+
+        // Get the form data
+        $.ajax({
+            method: "POST",
+            url: "https://reqres.in/api/users",
+            data: $formData,
+            success: function(response){
+                console.log(response);
+            }
+        });
+        
+        // Close modal and reset input values after AJAX request
+        $form[0].reset();
+        $modal.modal('toggle');
+    });
 });
